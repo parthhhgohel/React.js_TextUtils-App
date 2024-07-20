5e3b9c5b806c33335ee87fad7d24dc2781f7a061
parthhhgohel <parthgohel806@gmail.com>
2024-07-20 11:56:21 +0530

    Removing About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Alert from "./components/Alert";
 import TextForm from "./components/TextForm";
